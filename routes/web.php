e26d097772c2373054b26bf50fa190477e92f850
artyfyodrv <artyfyodrv@gmail.com>
2024-01-09 21:48:08 +0300

    #1 роуты, контроллер и методы регистрации

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,3 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::prefix('auth')->group(function () {
+    Route::get('register', [AuthController::class, 'register'])->name('register-page');
+    Route::post('register', [AuthController::class, 'registration'])->name('register-form');
+});
