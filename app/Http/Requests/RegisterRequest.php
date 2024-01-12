dce8dc672f76375e7d6e5acbdc1671016d4d0418
artyfyodrv <artyfyodrv@gmail.com>
2024-01-12 17:57:42 +0300

    #1 модель, валидация, миграция

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'login' => 'required|string|min:6|max:30',
+            'email' => 'required|email|max:64',
+            'password' => 'required|confirmed|max:60',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'login.required' => 'Поле логин не может быть пустым',
+            'login.string' => 'Поле логин имеет неверный формат',
+            'login.min' => 'Поле логин должно быть не менее 6 символов',
+            'login.max' => 'Поле логин превысило количество символов',
+            'email.required' => 'Поле почты не может быть пустым',
+            'email.email' => 'Поле логин имеет неверный формат',
+            'email.max' => 'Поле почты превысило количество символов',
+            'password.required' => 'Поле пароля не может быть пустым',
+            'password.confirmed' => 'Пароли не совпадают',
+            'password.max' => 'Поле пароля превысило количество символов'
+        ];
+    }
+}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,12 +10,24 @@
 <body>
 <form action="{{ route('register-form') }}" method="POST">
     @csrf
+
+    @error('login')
+    <span class="error">{{ $message }}</span>
+    @enderror
+
     <label for="username">Логин:</label>
-    <input type="text" id="username" name="username" required><br>
+    <input type="text" id="login" name="login" required><br>
+
+    @error('email')
+    <span class="error">{{ $message }}</span>
+    @enderror
 
     <label for="email">Эмейл:</label>
     <input type="email" id="email" name="email" required><br>
 
+    @error('password')
+    <span class="error">{{ $message }}</span>
+    @enderror
     <label for="password">Пароль:</label>
     <input type="password" id="password" name="password" required><br>
 
